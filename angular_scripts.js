19b789edb59ef52f8927cc8abdda790248d31f90
U-IPGNA\edward.ganuelas <edward.ganuelas@TORMCL-DWS8361.na.corp.ipgnetwork.com>
2015-09-22 20:02:54 +0000

    Working angular example

--- angular_scripts.js
+++ angular_scripts.js
@@ -1,9 +1,37 @@
 angular.module('app',[]);
-angular.module('app').controller("Controller", function(){
+angular.module('app').controller("Controller", function($http){
 	var vm = this;
 	vm.userName = '';
 	vm.platform = '';
-	vm.search = function(){
+	vm.data = [];
+	vm.search = function(){	
+		var apiKey = "";
+		var url = "http://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/"+vm.platform+"/"+vm.userName+"/";
 		
+		var responsePromise = $http.get(url,
+			{
+				headers:{
+					 "X-API-Key": apiKey
+				},
+				responseType: "json"
+			}
+		);
+	 	responsePromise.success(function(data){
+		 membershipId = data.Response[0].membershipId;
+		 var statsUrl = "http://www.bungie.net/Platform/Destiny/Stats/Account/"+vm.platform+"/"+membershipId+"/";
+		 var responseStats = $http.get(statsUrl,
+		 {
+			 headers:{
+					 "X-API-Key": apiKey
+				},
+				responseType: "json"
+		 }
+		 );
+		 responseStats.success(function(data){
+			console.log(data.Response.mergedAllCharacters.results.allPvP); 
+			vm.data.push(data.Response.mergedAllCharacters.results.allPvP.allTime);
+			
+		 });
+	 });
 	};
 });
