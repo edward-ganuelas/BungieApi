71b539554f6bd75d00a89de6751b7c56f6674216
U-IPGNA\edward.ganuelas <edward.ganuelas@TORMCL-DWS8361.na.corp.ipgnetwork.com>
2015-09-22 18:07:58 +0000

    Experimenting with Angular

---
+++ angular_scripts.js
@@ -0,0 +1,9 @@
+angular.module('app',[]);
+angular.module('app').controller("Controller", function(){
+	var vm = this;
+	vm.userName = '';
+	vm.platform = '';
+	vm.search = function(){
+		
+	};
+});
